56d0c96b524ad4b3b1658bb06a167ff30fe59e3e
DazedFury <jmtexas19@gmail.com>
2019-04-28 16:51:53 -0400

    feat: Added books for testing

--- src/BookFactory.java
+++ src/BookFactory.java
@@ -1,12 +1,17 @@
 public class BookFactory {
+    private String bookID = "0";
+
     //Return book based on type
-    public Book createBook(String type, String name, String author, String year, String bookID){
+    public Book createBook(String type, String name, String author, String year){
         switch (type) {
             case "Comic":
+                bookID = Integer.toString((Integer.parseInt(bookID) + 1));
                 return new Comic(name, author, year, bookID);
             case "Novel":
+                bookID = Integer.toString((Integer.parseInt(bookID) + 1));
                 return new Novel(name, author, year, bookID);
             case "Textbook":
+                bookID = Integer.toString((Integer.parseInt(bookID) + 1));
                 return new Textbook(name, author, year, bookID);
             default:
                 return null;
--- src/Library.java
+++ src/Library.java
@@ -7,6 +7,28 @@ public class Library {
     public static void main(String args[]){
         //Test stuff here
         Library lib = new Library();
-        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers", "Marvel", "1999", "123123"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers2", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers3", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers4", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers5", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Avengers6", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Iron Man", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Spiderman", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Captain America", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Comic", "Thor", "Marvel", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Novel", "Animal Farm", "George Orwell", "1945"));
+        lib.bookList.add(lib.bookFactory.createBook("Novel", "Murder Time", "John Sin", "1986"));
+        lib.bookList.add(lib.bookFactory.createBook("Novel", "Feel Something", "Laura Kian", "1999"));
+        lib.bookList.add(lib.bookFactory.createBook("Novel", "Rising Dragon", "Xiao Sen", "1976"));
+        lib.bookList.add(lib.bookFactory.createBook("Novel", "Martial World", "Lin Ming", "2004"));
+        lib.bookList.add(lib.bookFactory.createBook("Textbook", "Algebra 1", "Bob Barker", "2004"));
+        lib.bookList.add(lib.bookFactory.createBook("Textbook", "Algebra 2", "Bob Barker", "2004"));
+        lib.bookList.add(lib.bookFactory.createBook("Textbook", "Algebra 3", "Bob Barker", "2005"));
+        lib.bookList.add(lib.bookFactory.createBook("Textbook", "Calculus 1", "Bob Barker", "2005"));
+        lib.bookList.add(lib.bookFactory.createBook("Textbook", "Calculus 2", "Bob Barker", "2006"));
+
+
+
     }
 }
